77ae24179f1d30bbfcfb3a3bfafa6a58dc714089
Anton Larionov <anton2larionov@gmail.com>
2016-02-20 12:03:29 +0300

    decomposition of main function

--- src/main/scala-2.11/by/scalalab/Main.scala
+++ src/main/scala-2.11/by/scalalab/Main.scala
@@ -4,7 +4,6 @@ import java.nio.file.{Files, Paths}
 
 import by.scalalab.ip._
 
-import scala.collection.immutable.Seq
 import scala.io.Source
 import scala.util.{Failure, Try}
 
@@ -21,56 +20,22 @@ object Main {
         source.close; Stream.empty[String]
       }
 
-    val rangesIO = Try{ lines(Source.fromURL(getClass.getResource("/ranges.tsv"))) }
+    val rangesIO = Try { lines(Source.fromURL(getClass.getResource("/ranges.tsv"))) }
     val transactionsIO = Try { lines(Source.fromURL(getClass.getResource("/transactions.tsv"))) }
 
     // read, parse an IP, make ranges and segments
-    val segmentsTry = for {
-      stream <- rangesIO
-    } yield {
-      stream.par.map(_.split("""\t""")).filter(_.length == 2)
-        .map(parts => (parts(0).split("""-"""), parts(1)))
-        .map(pair => {
-          val (ips, name) = pair
-          for {
-            ip1 <- mkIPAddress(ips(0))
-            ip2 <- mkIPAddress(ips(1))
-            range <- Try(IPAddressRange(ip1, ip2))
-            segment <- Try(Segment(range, name))
-          } yield segment
-        })
-        .flatMap(_.toOption).seq
-    }
+    val segmentsTry = for { stream <- rangesIO }
+      yield mkSegments(stream)
 
     // read, parse user Id, make transactions
-    val transactionsTry = for {
-      stream <- transactionsIO
-    } yield {
-      stream.par.map(_.split("""\t""")).filter(_.length == 2)
-        .map(parts =>
-          for {
-            id <- Try(parts(0).toLong)
-            ip <- mkIPAddress(parts(1))
-          } yield Transaction(id, ip))
-        .flatMap(_.toOption).seq
-    }
+    val transactionsTry = for { stream <- transactionsIO }
+      yield mkTransactions(stream)
 
-    // search tree
-    val treeTry = for {s <- segmentsTry} yield SegmentTree(s)
-
-    // generation of the required information (userId -> list of segment's name)
+    // generation of the required information (userId -> seq of segment's name)
     val dataTry = for {
       segments <- segmentsTry
       transactions <- transactionsTry
-      tree <- treeTry
-    } yield {
-      for {action <- transactions} yield {
-        val seq = tree.segments(action.ip)
-        val res = if (seq.nonEmpty) seq.map(_.name).distinct else Seq("Unknown")
-
-        (action.userId, res)
-      }
-    }
+    } yield mkData(segments, transactions)
 
     // write data to file
     val writerTry = for {
@@ -79,25 +44,25 @@ object Main {
     } yield {
       try {
         for {
-          elem <- data
-          n <- elem._2
-        } writer.write(s"${elem._1}\t$n\n")
+          (id, names) <- data
+          name <- names
+        } writer.write(s"$id\t$name\n")
       } finally writer.close()
       writer
     }
 
     // errors
-    List(segmentsTry, transactionsTry, treeTry, dataTry, writerTry)
+    List(segmentsTry, transactionsTry, dataTry, writerTry)
       .distinct.filter(_.isFailure) match {
       case Nil => println("Program successfully finished without errors.")
       case list => list.foreach(println)
     }
   }
 
-  private val ipRegex = """^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$""".r
-
   /** Creates an [[by.scalalab.ip.IPAddress]] from any [[String]]. */
   def mkIPAddress(str: String): Try[IPAddress] = {
+
+    val ipRegex = """^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$""".r
 
     def parse(str: String) = str match {
       case ipRegex(v1, v2, v3, v4) => Try(IPAddress(v1.toInt, v2.toInt, v3.toInt, v4.toInt))
@@ -107,5 +72,44 @@ object Main {
     for {res <- parse(str)} yield res
   }
 
+  def mkSegments(stream: Stream[String]): Seq[Segment] = {
+    stream.par.map(_.split("""\t""")).filter(_.length == 2)
+      .map(parts => (parts(0).split("""-"""), parts(1)))
+      .map(pair => {
+        val (ips, name) = pair
+        for {
+          ip1 <- mkIPAddress(ips(0))
+          ip2 <- mkIPAddress(ips(1))
+          range <- Try(IPAddressRange(ip1, ip2))
+          segment <- Try(Segment(range, name))
+        } yield segment
+      })
+      .flatMap(_.toOption).seq
+  }
+
+  def mkTransactions(stream: Stream[String]): Seq[Transaction] = {
+    stream.par.map(_.split("""\t""")).filter(_.length == 2)
+      .map(parts =>
+        for {
+          id <- Try(parts(0).toLong)
+          ip <- mkIPAddress(parts(1))
+        } yield Transaction(id, ip))
+      .flatMap(_.toOption).seq
+  }
+
+  def mkData(segments: Seq[Segment],
+             transactions: Seq[Transaction],
+             defaultNW: String = "Unknown") = {
+    // search tree
+    val tree = SegmentTree(segments)
+    for {
+      action <- transactions
+    } yield {
+      val seq = tree.segments(action.ip)
+      val res = if (seq.nonEmpty) seq.map(_.name).distinct.sorted else Seq(defaultNW)
+
+      (action.userId, res)
+    }
+  }
 }
 
--- src/test/scala-2.11/by/scalalab/MainTest.scala
+++ src/test/scala-2.11/by/scalalab/MainTest.scala
@@ -1,12 +1,13 @@
 package by.scalalab
 
-import by.scalalab.ip.IPAddress
+import by.scalalab.ip._
 import org.scalatest.FunSuite
 
+import scala.io.Source
 import scala.util.{Failure, Success}
 
 /**
-  * Test of functions in [[by.scalalab.Main]].
+  * Tests of functions from [[by.scalalab.Main]].
   */
 class MainTest extends FunSuite {
 
@@ -25,7 +26,72 @@ class MainTest extends FunSuite {
       case Success(ip) => fail("Failed.")
       case Failure(e) => assert(e.isInstanceOf[IllegalArgumentException])
     }
+  }
 
+  test("testMkSegments") {
+    val strings =
+      "41.108.32.68-41.108.32.99\tNetwork39\n" +
+      "26.65.125.125-26.65.126.47\tNetwork33"
+
+    val stream = Source.fromString(strings).getLines().toStream
+
+    val segments = Main.mkSegments(stream)
+
+    assert(segments.size == 2)
+
+    val ips = segments.flatMap(s => Vector(s.range.ip1, s.range.ip2))
+
+    assert(ips.contains(IPAddress(26, 65, 125, 125)))
+
+    val names = segments.map(_.name)
+
+    assert(names.contains("Network39"))
+
+    assert(Main.mkSegments(Stream[String]("empty")).isEmpty)
+  }
+
+  test("testMkTransactions") {
+    val strings = "-6124327207679313045\t61.75.26.68\n" +
+      "4596635809354383891\t177.162.30.243"
+
+    val stream = Source.fromString(strings).getLines().toStream
+
+    val transactions = Main.mkTransactions(stream)
+
+    assert(transactions.size == 2)
+
+    val ips = transactions.map(_.userId)
+
+    assert(ips.contains(4596635809354383891L))
+
+    val names = transactions.map(_.ip)
+
+    assert(names.contains(IPAddress(61, 75, 26, 68)))
+
+    assert(Main.mkTransactions(Stream[String]("empty")).isEmpty)
+  }
+
+  test("testMkData") {
+    val transactions = Seq(
+      Transaction(123L, IPAddress(26, 65, 125, 125)),
+      Transaction(-987L, IPAddress(67, 4, 205, 1)),
+      Transaction(951L, IPAddress(152, 94, 78, 63))
+    )
+
+    val segments = Seq(
+      Segment(IPAddressRange(IPAddress(0, 0, 0, 0), IPAddress(127, 127, 127, 127)), "Nw0"),
+      Segment(IPAddressRange(IPAddress(25, 40, 129, 1), IPAddress(51, 17, 146, 12)), "Nw1"),
+      Segment(IPAddressRange(IPAddress(44, 79, 3, 209), IPAddress(79, 143, 177, 4)), "Nw2")
+    )
+
+    val data1 = Main.mkData(segments, transactions)
+
+    assert(data1.contains((123L, Seq("Nw0", "Nw1"))))
+    assert(data1.contains((-951L, Seq("Unknown"))))
+
+    val data2 = Main.mkData(segments, transactions, "NetworkZ")
+
+    assert(data2.contains((-951L, Seq("NetworkZ"))))
   }
 
 }
